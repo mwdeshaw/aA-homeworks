3adaccd505b973ef4edd7cea44242cacc6d8bab0
mwdeshaw <mwdeshaw@gmail.com>
2019-05-16 21:56:04 -0700

    Optimize methods in Stack class

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,17 +1,24 @@
 class Stack
     def initialize
-
+        @stack = []
     end
 
     def push(el)
+        @stack << el
+
+        self
     end 
 
     def pop
+        raise "Stack is empty" if self.empty?
+        @stack.pop
 
+        self
     end
 
     def peek
-
+        raise "Stack is empty" if self.empty?
+        return @stack.last
     end
 
 end
