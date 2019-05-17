a8594ff01e892e824936e512a56b6c85c07781aa
mwdeshaw <mwdeshaw@gmail.com>
2019-05-16 22:12:42 -0700

    Remove test cases, as Stack passes them

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -24,20 +24,3 @@ class Stack
     end
 
 end
-
-
-#testing
-# test_stack = Stack.new
-# p test_stack    # []
-# test_stack.push(1)
-# p test_stack # [1]
-# test_stack.push(2)
-# test_stack.push(3)
-# test_stack.push(4)
-# p test_stack # [1, 2, 3, 4]
-# test_stack.pop
-# test_stack.pop
-# p test_stack # [1, 2]
-# test_stack.push(3) 
-# p test_stack #[1, 2, 3]
-# p test_stack.peek #3
