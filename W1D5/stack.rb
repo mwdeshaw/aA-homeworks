4df6ea247183b725df4317dad8fe2a7e96cb471d
mwdeshaw <mwdeshaw@gmail.com>
2019-05-16 22:08:45 -0700

    Add #inspect method to Stack, create new Stack instance and run tests

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -10,15 +10,36 @@ class Stack
     end 
 
     def pop
-        raise "Stack is empty" if self.empty?
+        raise "Stack is empty" if @stack.empty?
         @stack.pop
 
         self
     end
 
     def peek
-        raise "Stack is empty" if self.empty?
-        return @stack.last
+        raise "Stack is empty" if @stack.empty?
+        @stack.last
+    end
+
+    def inspect
+        @stack.inspect
     end
 
 end
+
+#testing
+# test_stack = Stack.new
+# p test_stack    # []
+# test_stack.push(1)
+# p test_stack # [1]
+# test_stack.push(2)
+# test_stack.push(3)
+# test_stack.push(4)
+# p test_stack # [1, 2, 3, 4]
+# test_stack.pop
+# test_stack.pop
+# p test_stack # [1, 2]
+# test_stack.push(3) 
+# p test_stack #[1, 2, 3]
+# p test_stack.peek #3
+
