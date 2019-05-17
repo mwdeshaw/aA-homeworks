a4544745c9d21fd6c1626d9a8e7aca7c8b14af45
mwdeshaw <mwdeshaw@gmail.com>
2019-05-16 22:28:28 -0700

    Remove test cases after all pass

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -24,25 +24,3 @@ class Queue
     end
 
 end
-
-
-#testing
-test_queue = Queue.new
-p test_queue #[]
-test_queue.enqueue(1)
-p test_queue #[1]
-test_queue.enqueue(2)
-p test_queue #[1, 2]
-test_queue.enqueue(3)
-test_queue.enqueue(4)
-p test_queue #[1, 2, 3, 4]
-test_queue.dequeue
-p test_queue # [2, 3, 4]
-test_queue.dequeue
-test_queue.enqueue(5)
-test_queue.enqueue(6)
-p test_queue #[3, 4, 5, 6]
-p test_queue.peek #3
-test_queue.dequeue
-p test_queue.peek #4
-p test_queue #[4, 5, 6]
