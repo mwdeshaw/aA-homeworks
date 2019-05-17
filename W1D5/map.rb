c54a3ebece7e2e512c2c38de161e9e22e0b32dd4
mwdeshaw <mwdeshaw@gmail.com>
2019-05-16 22:33:12 -0700

    Define methods for Map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,3 +1,25 @@
 class Map
+    def initialize
+        @map = Array.new { Array.new(2) }
+    end
+
+    def set(key, val)
+
+    end
+
+    def get(key)
+    
+    end
+
+    def delete(key)
+    end
+
+    def show
+
+    end
+
+    def inspect
+        @map.inspect
+    end
 
 end
