f6039fcc116563ebbf46c42eaa5db52ddf6643f7
mwdeshaw <mwdeshaw@gmail.com>
2019-05-16 23:16:57 -0700

    Add test cases for Map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -4,29 +4,41 @@ class Map
     end
 
     def set(key, val)
-        if @map.any? { |pair| pair.first == key }
-            @map.each { |pair| pair.last = val if pair.first == key }
+        if @map.any? { |pair| pair[0] == key }
+            @map.each { |pair| pair[-1] = val if pair[0] == key }
         else
             @map << [key, val]
         end
-
-    self
     end
 
     def get(key)
         @map.each do |pair|
-            return pair if pair.first == key
+            return pair if pair[0] == key
         end
 
         raise "Map does not contain key"
     end
 
     def delete(key)
-        @map.reject { |pair| pair.first == key }
+        @map.reject! { |pair| pair[0] == key }
     end
 
     def show
-        p self
+        p self 
     end
 
 end
+
+#testing
+my_map = Map.new
+my_map.set("k1", 1)
+my_map.show
+my_map.set("k2", 2)
+my_map.set("k3", 3)
+my_map.show
+my_map.set("k2", 3)
+p my_map.get("k2")
+my_map.show
+my_map.get("k3")
+my_map.delete("k3")
+my_map.show
