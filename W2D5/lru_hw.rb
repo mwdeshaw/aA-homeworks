6a4b8746a898528e37c56b83d400d338c2c1c2c4
mwdeshaw <mwdeshaw@gmail.com>
2019-05-23 22:12:19 -0700

    Complete LRU hw

--- W2D5/lru_hw.rb
+++ W2D5/lru_hw.rb
@@ -9,44 +9,40 @@ class LRUCache
     end
 
     def add(el)
-        unless cache.include?(el)
-            new_cache = cache.unshift(el)
+        if cache.include?(el)
+            cache.delete(el)
+            cache << el
+        elsif count >= size
+            cache.shift
+            cache << el
         else
-            idx = cache.index(el)
-            new_cache = cache.delete_at(idx)
-            new_cache.unshift(el)
+            cache << el
         end
-
-        slicer(new_cache)
+        cache
     end
 
     def show
-        cache.last
+        p cache
     end
 
     private
     attr_accessor :size
     attr_accessor :cache
-    def slicer(cache)
-        cache[0..size]
-    end
 
   end
 
 #testing
-johnny_cache = LRUCache.new(4)
-johnny_cache.add("I walk the line")
-johnny_cache.add(5)
-johnny_cache.count # => returns 2
-johnny_cache.add([1,2,3])
-johnny_cache.add(5)
-johnny_cache.add(-5)
-johnny_cache.add({a: 1, b: 2, c: 3})
-johnny_cache.add([1,2,3,4])
-johnny_cache.add("I walk the line")
-johnny_cache.add(:ring_of_fire)
-johnny_cache.add("I walk the line")
-johnny_cache.add({a: 1, b: 2, c: 3})
+# johnny_cache = LRUCache.new(4)
+# johnny_cache.add("I walk the line")
+# johnny_cache.add(5)
+# johnny_cache.count # => returns 2
+# johnny_cache.add([1,2,3])
+# johnny_cache.add(5)
+# johnny_cache.add(-5)
+# johnny_cache.add({a: 1, b: 2, c: 3})
+# johnny_cache.add([1,2,3,4])
+# johnny_cache.add("I walk the line")
+# johnny_cache.add(:ring_of_fire)
+# johnny_cache.add("I walk the line")
+# johnny_cache.add({a: 1, b: 2, c: 3})
 johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
-
- [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
